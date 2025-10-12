541729db0aeac534492661333061dd0b7c5332ef
vis2hnu <128176732+vis2hnu@users.noreply.github.com>
2025-10-12 14:17:01 +0530

    Add files via upload
    Day1 Assignment

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "John Doe";
+		int age = 30;
+		double salary = 55000.75;
+		char gender = 'M';
+	    boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210l;
+		int pinCode = 56001;
+		byte luckyNumber = 7;
+		System.out.println(name + " " + age  + " " + salary  + " " + gender  + " " + acceptTerms  + " " + rating  + " " + mobileNumber  + " " + pinCode  + " " +luckyNumber);
+
+	} 
+
+}
